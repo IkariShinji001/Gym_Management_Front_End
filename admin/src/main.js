e3f559d9ad3e0ad565e6d7eb568bb5715e845a67
chickenlee2003 <tuankiet191103@gmail.com>
2024-09-27 10:39:56 +0700

    Kiêt

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,40 +1,33 @@
 // FILE: main.js
 
 import { createApp } from "vue";
-import { Quasar, Loading, Notify } from "quasar";
-import { QSpinnerCube } from "quasar";
-import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
+import { Quasar, Loading, Dialog } from "quasar";
+
+// Import icon libraries
 import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
 import "quasar/src/css/index.sass";
 
+// Assumes your root component is App.vue
+// and placed in same folder as main.js
 import App from "./App.vue";
-import router from "./router/index";
-
-const options = {
-  timeout: 3000,
-};
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+import router from "./router";
 
 const myApp = createApp(App);
 
-myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
-});
-
-myApp.use(router);
-myApp.use(Toast, options);
-  plugins: {
-    Loading,
-  },
-  config: {
-    loading: {
-      spinner: QSpinnerCube,
-      message: "Loading...",
-    },
-  },
-});
-myApp.use(router);
-myApp.use(Toast, options);
+myApp
+  .use(Quasar, {
+    plugins: {
+      Loading,
+      Dialog,
+    }, // import Quasar plugins and add here
+  })
+  .use(router)
+  .use(Toast);
 
 // Assumes you have a <div id="app"></div> in your index.html
 myApp.mount("#app");
